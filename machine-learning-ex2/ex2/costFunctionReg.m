3d15976052560e80b460ed1d3d52b787e7a94025
Łukasz Chmielewski <luchmiel@microsoft.com>
2015-11-18 19:37:18 +0100

    Regularization submited.

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -8,20 +8,17 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
-
+grad = ones(size(theta));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
-
-
-
-
-
-
+predictions = sigmoid(X*theta);
+J = sum( -y.*log(predictions) - (1 - y).*log(1 - predictions))/m + lambda*sum(theta(2:end).^2)/(2*m);
+errors = (predictions - y);
+grad = (X'*errors + lambda*theta)/m;
+grad(1) = (predictions - y)'*X(:,1)/m;
 % =============================================================
 
 end
