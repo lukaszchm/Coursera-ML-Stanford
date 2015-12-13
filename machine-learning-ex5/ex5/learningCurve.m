321b08b169c40af7bae2a6120fe939f4c40b5fb0
Łukasz Chmielewski <luchmiel@microsoft.com>
2015-12-13 18:37:04 +0100

    Learning Curve submited.

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,6 +53,13 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+for i = 1:m
+    theta = trainLinearReg(X(1:i,:), y(1:i), lambda);
+    [trainingCost,~] = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
+    [cvCost,~] = linearRegCostFunction(Xval,yval, theta, 0);
+    error_train(i) = trainingCost;
+    error_val(i) = cvCost;
+end
 
 
 
