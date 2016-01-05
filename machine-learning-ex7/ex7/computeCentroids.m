5240dd74d93150020265b4ffe3ef97fdb260ce8c
Łukasz Chmielewski <luchmiel@microsoft.com>
2016-01-05 15:12:23 +0100

    Compute Centroid Means (k-Means) submited;

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -26,7 +26,10 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
+for i = 1:K
+    x = X(idx == i, :);
+    centroids(i, :) = (1 / size(x, 1)) * sum(x, 1);
+end;
 
 
 
